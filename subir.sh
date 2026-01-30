75d049cb009e612e9cd4ab1275a6107816eb1edc
adderlyRodriguez <adderlyconstrucciones@gmail.com>
2026-01-30 03:24:17 -0300

    carga respaldo

--- subir.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Primer commit"
-# Crea un repo en GitHub.com y sigue las instrucciones para 'push'
-git remote add origin https://github.com/felixjimenez/adderly.git
-git push -u origin master
